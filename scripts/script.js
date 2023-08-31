3b905ef41569800c186461dedd322e3be3c4c0ef
AdityaChowdhury2 <adityac5102@gmail.com>
2023-09-01 03:00:03 +0600

    main task done

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,41 @@
-const loadCategories = async () => {
+let isSort = false;
+
+const handleSort = () => {
+    isSort = !isSort;
+    loadCategories(isSort);
+}
+const loadCategories = async (isSort) => {
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const data = await res.json();
     const categories = data.data;
 
     const categoryContainer = document.getElementById('category-container');
-
+    categoryContainer.innerText = ''
     categories.forEach((category) => {
         const a = document.createElement('a');
         a.classList = 'btn tab  btn-sm';
         a.innerHTML = `
             ${category.category}
         `
-        a.onclick = () => loadVideos(category.category_id);
+        a.onclick = (e) => {
+            e.target.parentElement.querySelector('.tab-active').classList.remove('tab-active', 'bg-primary-color', 'text-white', 'border-none', 'hover:bg-dark-primary-color')
+            e.target.classList.add('tab-active', 'bg-primary-color', 'text-white', 'border-none', 'hover:bg-dark-primary-color');
+            loadVideos(category.category_id, isSort)
+        };
         categoryContainer.appendChild(a);
     })
 
     categoryContainer.firstElementChild.classList.add('tab-active', 'bg-primary-color', 'text-white', 'border-none', 'hover:bg-dark-primary-color');
 
-
+    loadVideos('1000', isSort);
 };
 
 
-const loadVideos = async categoryId => {
-    console.log('Clicked', categoryId);
-
+const loadVideos = async (categoryId, isSort) => {
+    // console.log('Clicked', categoryId);
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await res.json();
     const videos = data.data;
-    console.log(videos);
 
     const videoContainer = document.getElementById('video-container');
     videoContainer.innerText = '';
@@ -40,23 +48,23 @@ const loadVideos = async categoryId => {
         <img src='images/Icon.png'>
         <h2 class='font-bold text-3xl'>Oops!! Sorry, There is no content here.</h2> 
     `
-    noContentDiv.classList = 'mt-52 flex flex-col items-center text-center'
+    noContentDiv.classList = 'mt-52 flex flex-col items-center text-center';
+    console.log(isSort);
+    isSort && videos.sort((a, b) => b.others.views.slice(0, -1) - a.others.views.slice(0, -1))
 
+    videos.length ?
 
-    videos.length ? videos.forEach((video) => {
+        videos.forEach(({ thumbnail, title, authors, others, }) => {
 
-        const div = document.createElement('div')
-        div.innerHTML = `
-        <div class="relative">
-        <img src="https://i.ibb.co/L1b6xSq/shape.jpg" alt="" class="rounded-xl">
-        <p
-            class="absolute p-[5px] bottom-2 right-2 text-white text-xs bg-dark-secondary-color rounded-md">
-            3 hrs
-            56
-            mins
-            ago</p>
-    </div>
-
+            const div = document.createElement('div')
+            div.classList = 'card';
+            div.innerHTML = `
+        <figure class="relative">
+            <img src=${thumbnail} alt="" class="w-full h-48 rounded-xl">
+             
+            <p class="absolute p-[5px] bottom-2 right-2 text-white text-xs bg-dark-secondary-color rounded-md ${Number(others.posted_date) ? '' : 'hidden'} "> ${fixTime(others.posted_date)}
+            </p>
+        </figure >
     <div class="flex gap-3 mt-5">
         <div class="avatar">
             <div class="w-10 h-10 rounded-full">
@@ -64,18 +72,19 @@ const loadVideos = async categoryId => {
             </div>
         </div>
         <div>
-            <h3 class="font-bold">Building a winning UX strategy Using the kano Model</h3>
+            <h3 class="font-bold">${title}</h3>
             <div class="flex gap-2">
-                <p class="text-sm text-gray-400">Awlad Hossain</p>
-                <img src="images/verified.png">
+                <p class="text-sm text-gray-400">${authors[0].profile_name}</p>
+                <img class="${authors[0]?.verified === true ? '' : 'hidden'}" src="./images/verified.png">
             </div>
-            <p class="text-sm text-gray-400"><span>91</span>k views</p>
+            <p class="text-sm text-gray-400"><span>${others.views}</span> views</p>
         </div>
     </div>
-        `
-
-        videoContainer.appendChild(div);
-    }) : noContentContainer.appendChild(noContentDiv)
+`
+            // console.log(authors[0]?.verified === true, authors[0].profile_name);
+            videoContainer.appendChild(div);
+        })
+        : noContentContainer.appendChild(noContentDiv)
 
 
 
@@ -83,7 +92,13 @@ const loadVideos = async categoryId => {
 
 
 };
+loadCategories(isSort);
 
 
-loadCategories();
-loadVideos('1000');
+const fixTime = (sec) => {
+    const hours = Math.trunc(sec / 3600);
+    const mins = Math.trunc((sec % 3600) / 60);
+    return `${hours}hrs ${mins}mins ago`;
+    // console.log(`${ hours }hrs ${ mins }mins ago`);
+}
+
